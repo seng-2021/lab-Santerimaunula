a4967d2a80169ef383f397adb29784075b9c2737
Santerimaunula <124351159+Santerimaunula@users.noreply.github.com>
2024-02-15 11:36:53 +0200

    Update mycrypt.py

--- lab2/mycrypt.py
+++ lab2/mycrypt.py
@@ -9,16 +9,17 @@ def encode(s):
     if len(s) > 1000:
         raise ValueError
     for c in s:
-        if c.isalpha():
+        if c.isalpha() and ord(c) <= ord('z'):
             if c.islower():
                 c=c.upper()
             # Rot13 the character for maximum security
             crypted+=codecs.encode(c,'rot13')
         elif c in digitmapping:
           crypted+=digitmapping[c]
-
-    return crypted
+        else:
+            raise ValueError
+    return crypted[0:origlen]
 
 def decode(s):
-    return s
+    return s encode(s).lower()
 
